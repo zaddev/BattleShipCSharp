8736b6ca73d5350d4b4f7eff43135c14b542fa8d
barld <bakeds@gmail.com>
2014-11-10 18:30:38 +0100

    added diferent ships

--- ZeeslagLib/Ship.cs
+++ ZeeslagLib/Ship.cs
@@ -5,20 +5,12 @@ using System.Text;
 
 namespace ZeeslagLib
 {
-    public class Ship
+    public abstract class Ship
     {
         public event EventHandler OnShipFound;
-    
-        public Byte Length
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-            }
-        }
+
+        public abstract Byte Length { get; }
+        public abstract string Name { get; } 
 
         public Direction Direction
         {
@@ -40,6 +32,93 @@ namespace ZeeslagLib
             set
             {
             }
+        }
+
+        
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+    /// <summary>
+    /// each player have one
+    /// </summary>
+    public class AicraftCarrier : Ship
+    {
+        public override byte Length
+        {
+            get { return 5; }
+        }
+
+        public override string Name
+        {
+            get { return "aircraft Carrier"; }
+        }
+    }
+
+    /// <summary>
+    /// each players has two
+    /// </summary>
+    public class BattleShip : Ship
+    {
+        public override byte Length
+        {
+            get { return 4; }
+        }
+
+        public override string Name
+        {
+            get { return "battle ship"; }
+        }
+    }
+
+    /// <summary>
+    /// each player has three
+    /// </summary>
+    public class SubMarine : Ship
+    {
+        public override byte Length
+        {
+            get { return 3; }
+        }
+
+        public override string Name
+        {
+            get { return "submarine"; }
+        }
+    }
+
+    /// <summary>
+    /// each player has three
+    /// </summary>
+    public class Destroyer : Ship
+    {
+        public override byte Length
+        {
+            get { return 3; }
+        }
+
+        public override string Name
+        {
+            get { return "destroyer"; }
+        }
+    }
+
+    /// <summary>
+    /// each player has four
+    /// </summary>
+    public class PatrolBoat : Ship
+    {
+        public override byte Length
+        {
+            get { return 2; }
+        }
+
+        public override string Name
+        {
+            get { return "patrol boat"; }
         }
     }
 }
