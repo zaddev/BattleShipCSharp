a097b904c4363ce8fdfb06bd89a4c47fe5c6af30
barld <bakeds@gmail.com>
2014-11-13 13:23:40 +0100

    game settings

---
+++ ZeeslagLib/BattleShipSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace ZeeslagLib
+{
+    /// <summary>
+    /// all configuration settings for the Battleship game
+    /// </summary>
+    public static class BattleShipSettings
+    {
+        /// <summary>
+        /// triggers when ConnectionURL change
+        /// </summary>
+        public static event EventHandler OnConnectionURLChange;
+
+        /// <summary>
+        /// url where the node server is hosting
+        /// </summary>
+        public static string ConnectionURL
+        {
+            get { return System.Configuration.ConfigurationManager.AppSettings["ConnectionURL"]; }
+            set
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                config.AppSettings.Settings["ConnectionURL"].Value = value;
+                if(OnConnectionURLChange!=null)D:\data barld\Documents\BattleShipCSharp\ZeeslagLib\BattleShipSettings.cs
+                    OnConnectionURLChange(value, new EventArgs());
+
+                //save to apply changes
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+        }
+    }
+}
